362afaf5a53b1147ba63f19033ff2fa7b46c2051
Matt M <matt.d.m@hotmail.com>
2019-11-11 13:01:21 -0600

    🛠 fix eslint issue, added redirect rules

--- react-app/src/Auth.jsx
+++ react-app/src/Auth.jsx
@@ -1,4 +1,3 @@
-/* eslint no-restricted-globals: 0 */
 import auth0 from 'auth0-js';
 import jwtDecode from "jwt-decode";
 
@@ -31,10 +30,10 @@ export default class Auth {
                 localStorage.setItem("access_token", authResults.accessToken);
                 localStorage.setItem("id_token", authResults.idToken);
                 localStorage.setItem("expires_at", expiresAt);
-                location.hash = "";
-                location.pathname = LOGIN_SUCCESS_PAGE;
+                window.location.hash = "";
+                window.location.pathname = LOGIN_SUCCESS_PAGE;
             } else if (err) {
-                location.pathname = LOGIN_FAILURE_PAGE;
+                window.location.pathname = LOGIN_FAILURE_PAGE;
                 console.log(err)
             }
         });
@@ -49,7 +48,7 @@ export default class Auth {
         localStorage.removeItem("access_token");
         localStorage.removeItem("id_token");
         localStorage.removeItem("expires_at");
-        location.pathname = LOGIN_FAILURE_PAGE;
+        window.location.pathname = LOGIN_FAILURE_PAGE;
     }
 
     getProfile() {
