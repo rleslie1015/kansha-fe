4106a3966e64f52fa64d0b0c720fa10aa40e44f0
leslie <rleslie1015@gmail.com>
2019-11-14 15:24:05 -0600

    finished form

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import Onboarding from './Onboarding';
 
 export const App = () => {
-    return (
-        <div className="App">
-            <Route exact path="/" component={Login} />
-            <Route path="/home" component={Home} />
-        </div>
-    );
+	return (
+		<div className="App">
+			<Route exact path="/" component={Login} />
+			<Route path="/home" component={Home} />
+			<Route path="/onboarding" component={Onboarding} />
+		</div>
+	);
 };
---
+++ react-app/src/Onboarding.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography, Paper, Button, FormControl, TextField, Select, MenuItem, InputLabel } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+    paper: {
+        marginTop: '1rem',
+    },
+    typo: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '1rem .5rem',
+    },
+    form: {
+        width: '100%',
+        marginTop: '1rem'
+    },
+    button: {
+        width: '10%',
+        margin: '1rem 45%',
+        backgroundColor: '#349AFA',
+        color: 'white',
+        textDecoration: 'none',
+    },
+}));
+
+export default function Onboarding() {
+    const classes = useStyles();
+    const [ role, setRole ] = useState("");
+
+    const handleChange = event => {
+        setRole(event.target.value);
+      };
+
+    return (
+        <div id="App" className={classes.root}>
+            <CssBaseline />
+            <Container fixed>
+                <Paper className={classes.paper}>
+                    <Typography className={classes.typo} variant="h5">
+                        Welome, Please specify your role. 
+                    </Typography>
+                    <FormControl>
+                        <TextField 
+                            label="First Name"
+                        />
+                        <TextField 
+                            label="Last Name"
+                        />
+                        <TextField 
+                            label="Job Title"
+                        />    
+                        <FormControl>
+                            <InputLabel value={role} id="Role-label"><em>Select A Role</em></InputLabel> 
+                            <Select     
+                                labelId="Role-label"
+                                id="Role"
+                                value={role}
+                                onChange={handleChange}
+                            >
+                                <MenuItem value="admin">Admin</MenuItem> 
+                                <MenuItem value="mod">Mod</MenuItem> 
+                                <MenuItem value="standard">Standard</MenuItem> 
+                            </Select>
+                        </FormControl>
+                        <TextField 
+                            label="Organization"
+                        />
+                        <TextField 
+                            label="Department"
+                        />
+                        <Button
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                        >
+                            Register
+                        </Button>
+                    </FormControl>
+                </Paper>
+            </Container>
+        </div>
+    );
+}
