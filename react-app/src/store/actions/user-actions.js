5f99c97650ac45da6eb2f6e0dae113d6c69b871f
leslie <rleslie1015@gmail.com>
2019-11-14 15:53:16 -0600

    ready for redux functionality

--- react-app/src/store/actions/user-actions.js
+++ react-app/src/store/actions/user-actions.js
@@ -1,4 +1,16 @@
+import axiosWithAuth from "../../utils/axiosWithAuth";
+
 export const USER_LOGIN = "USER_LOGIN"
+export const USER_ONBOARDING_START = "USER_ONBOARDING_START"
+export const USER_ONBOARDING_SUCCESS = "USER_ONBOARDING_SUCCESS"
+export const USER_ONBOARDING_FAILURE = "USER_ONBOARDING_FAILURE"
+
+
+
 export const login = (userProfile) => dispatch => {
     dispatch({ type: USER_LOGIN, payload: userProfile })   
+}
+
+export const onboard = () => dispatch => {
+    dispatch({ type: USER_ONBOARDING_START })
 }
