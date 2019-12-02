cf76857952eafa431dc1ab414b6572aa435c3704
Sydney Crumley <srsimps19@gmail.com>
2019-12-02 14:49:13 -0600

    pulled down

--- react-app/src/tests/Profile.spec.js
+++ react-app/src/tests/Profile.spec.js
@@ -1,1 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Profile from '../Profile';
+import { Drawer, IconButton } from '@material-ui/core';:
