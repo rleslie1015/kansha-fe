2f6288c0fb05dda91f1c4ac6b1a2123193b84272
Ty Lippe <tylippe94@gmail.com>
2019-12-02 17:57:17 -0500

    styling almost completed/ need to style dropdown

--- react-app/src/Onboarding.jsx
+++ react-app/src/Onboarding.jsx
@@ -3,37 +3,60 @@ import { onboard } from './store/actions/user-actions';
 import { connect } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography, Paper, Button, FormControl, TextField, MenuItem, Select } from '@material-ui/core';
+import logo from './assets/logo38.png';
+import kanshaLogo from './assets/logo39.png';
+import { Container, Typography, Paper, Button, FormControl, TextField, MenuItem, Select, Box, InputLabel } from '@material-ui/core';
 import 'typeface-montserrat';
 import 'typeface-roboto';
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
 		minHeight: '100vh',
 		backgroundColor: '#26242D',
-    },
-    onboardContainer: {
+	},
+	imageContainer: {
+		width: '48%',
+	},
+	kanshaLogo: {
+		width: '30%',
+	},
+	logo: {
+		width: '90%',
+		height: 'auto',
 		marginTop: '1rem',
+		marginLeft: '1rem',
+	},
+	formContainer: {
+		width: '48%',
+	},
+    onboard: {
+		display: 'flex',
+		flexDirection: 'column',
+		margin: '7rem 3rem 0 3rem',
+		width: '80%',
+		height: '80%',
 		backgroundColor: '#2D2C35',
-		boxShadow: '0px 0px 25px rgba(33, 32, 40, 0.1)',
+		// boxShadow: '0px 0px 25px rgba(33, 32, 40, 0.1)',
 		borderRadius: '2px',
+		padding: '2rem 2rem',
     },
     getStarted: {
         display: 'flex',
         justifyContent: 'center',
-		padding: '1rem .5rem',
+		padding: '1rem .5rem 3rem .5rem',
 		color: '#EE4D71',
 		fontFamily: 'Montserrat',
 		fontStyle: 'normal',
 		fontWeight: '600',
-		fontSize: '24px',
+		fontSize: '42px',
 		lineHeight: '24px',
 		letterSpacing: '0.15px',
 	},
 	textField: {
-		width: '200',
+		margin: '.5rem',
+		width: '100%',
 			'& input:valid + fieldset': {
 			  borderColor: 'rgba(255, 255, 255, 0.7)',
 			  borderWidth: '2',
@@ -49,12 +72,16 @@ const useStyles = makeStyles(theme => ({
 				fontFamily: 'Montserrat',
 				fontStyle: 'normal',
 				fontWeight: 'normal',
-				fontSize: '16px',
+				fontSize: '20px',
 				lineHeight: '20px',
 			},
 	},
+	select: {
+		// color: '#FFFFFF',
+	},
 	label: {
 		color: 'rgba(255, 255, 255, 0.7)',
+		fontSize: '24px',
 	},
 	input: {
 		color: '#FFFFFF',
@@ -62,14 +89,26 @@ const useStyles = makeStyles(theme => ({
 		fontFamily: 'Montserrat',
 		fontStyle: 'normal',
 		fontWeight: 'normal',
-		fontSize: '16px',
+		fontSize: '24px',
 		lineHeight: '20px',
 	},
-    form: {
-        width: '100%',
+	twoInput: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		width: '100%',
+		padding: '1rem',
+	},
+	oneInput: {
+		display: 'flex',
+		justifyContent: 'center',
+		width: '100%',
+		padding: '1rem',
 	},
     button: {
-        width: '100%',
+		width: '70%',
+		fontSize: '24px',
+		margin: '2rem 6rem',
 		borderRadius: '0',
 		backgroundColor: '#2D2C35',
 		boxShadow: 'none',
@@ -97,12 +136,17 @@ function Onboarding(props) {
     return (
         <div id="App" className={classes.root}>
             <CssBaseline />
-            <Container fixed>
-                <Paper className={classes.onboardContainer}>
+			<Container className={classes.imageContainer}>				
+				<img src={kanshaLogo} alt='Kansha Logo' className={classes.kanshaLogo} />
+				<img src={logo} alt='Kansha Logo People' className={classes.logo} />
+			</Container>
+			<Container className={classes.formContainer}>
+                <Paper className={classes.onboard}>
                     <Typography className={classes.getStarted} variant="h5">
                         Let's Get Started! 
                     </Typography>
                     <FormControl>
+						<Box className={classes.twoInput}>
                         <TextField 
 							label="First Name*"
 							placeholder="e.g. Jane"
@@ -133,6 +177,8 @@ function Onboarding(props) {
 								className: classes.label
 							}}
                         />
+						</Box>
+						<Box className={classes.twoInput}>
                         <TextField 
 							label="Job Title*"
 							placeholder="e.g. Manager"
@@ -148,27 +194,32 @@ function Onboarding(props) {
 								className: classes.label
 							}}
                         />    
-						<TextField
-          					select
-							label="Select a Role*" 
-							defaultValue="standard"
-							className={classes.textField}
-							value={form.role}
-							onChange={handleChange}
-							margin="normal"
-							InputProps={{
-								className: classes.input
-							  }}
-							InputLabelProps={{
-								className: classes.label
-							}}
-							variant="outlined">
-								<MenuItem value="standard">Standard</MenuItem> 
-                                <MenuItem value="mod">Mod</MenuItem> 
-                                <MenuItem value="admin">Admin</MenuItem> 
-        				</TextField>
+						<FormControl className={classes.textField}>
+							<InputLabel id='role-label'>Select a Role</InputLabel>
+							<Select 
+								labelId='role-label'
+								defaultValue="standard"
+								value={form.role}
+								onChange={handleChange}
+								margin="normal"
+								variant="outlined"
+								className={classes.select}
+								InputProps={{
+									className: classes.input
+								  }}
+								InputLabelProps={{
+									className: classes.label
+								}}
+								>
+									<MenuItem value="standard">Standard</MenuItem> 
+									<MenuItem value="mod">Mod</MenuItem> 
+									<MenuItem value="admin">Admin</MenuItem> 
+        					</Select>
+							</FormControl>
+						</Box>
+						<Box className={classes.oneInput}>
                         <TextField 
-							label="Organization"
+							label="Organization*"
 							placeholder="Organization Name"
 							className={classes.textField}
 							variant="outlined"
@@ -182,6 +233,8 @@ function Onboarding(props) {
 								className: classes.label
 							}}
                         />
+						</Box>
+						<Box className={classes.oneInput}>
                         <TextField 
 							label="Department"
 							placeholder="e.g. Marketing Department"
@@ -197,6 +250,7 @@ function Onboarding(props) {
 								className: classes.label
 							}}
                         />
+						</Box>
                         <Button
                             className={classes.button}
                             variant="contained"
