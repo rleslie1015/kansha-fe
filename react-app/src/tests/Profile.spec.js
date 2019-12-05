d424835811b5382d6396c34a8e7bb3dd2bd66c08
Sydney Crumley <srsimps19@gmail.com>
2019-12-05 11:21:40 -0600

    added 2 tests to profile

--- react-app/src/tests/Profile.spec.js
+++ react-app/src/tests/Profile.spec.js
@@ -3,26 +3,18 @@ import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Profile from '../Components/Profile/Profile';
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Profile />', () => {
-    it('it should find 3 containers in the profile', () => {
-        const wrapper = shallow(<Profile />);
-        expect(wrapper.find(Container).toHaveLength(3));
-      });
+    it('it should find a container in the profile', () => {
+        const wrapper = shallow(<provider><Profile /></provider>);
+        expect(wrapper.find(Container));
+    });
 
-    // it('it should fire a click event to button', () => {
-    //     const wrapper = shallow(<Workspace_Card team={fitleredTeam} />);
-
-    //     wrapper.find(Button).first().simulate('click');
-    //     expect(wrapper.prop('onClick'))
-    // })
-
-    // it('it should find seeded user data', () => {
-    //     const wrapper = shallow(<Workspace_Card team={fitleredTeam} />);
-
-    //     wrapper.find(Typography).find('Dev God')
-    // })
+    it('', () => {
+      const wrapper = shallow(<provider><Profile /></provider>);
+      expect(wrapper.exists(Typography))
+    });
 })
