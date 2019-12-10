b04e42ff68c9e1dec699eeeced5b1299daa6865e
Matt M <matt.d.m@hotmail.com>
2019-12-10 12:37:31 -0600

    🧹 removed eslint warnings

--- react-app/src/landing/Landing.js
+++ react-app/src/landing/Landing.js
@@ -2,13 +2,7 @@ import React from 'react';
 import Main from './Main';
 import Footer from './Footer'
 import Header from './Header';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundColor: '#26242D'
-    }
-}))
 
 export const Landing = () => {
 
--- react-app/src/landing/NavMenu.js
+++ react-app/src/landing/NavMenu.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
 import { Button, Menu, MenuItem } from '@material-ui/core';
-import MenuIcon from "@material-ui/icons/Menu";
+import MenuIcon from '@material-ui/icons/Menu';
 import Auth from '../utils/auth';
 import { makeStyles } from '@material-ui/core/styles';
 import 'typeface-montserrat';
@@ -9,66 +8,62 @@ import 'typeface-montserrat';
 const auth = new Auth();
 
 const useStyles = makeStyles(theme => ({
-  hideNav: {
-    padding: theme.spacing(1),
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      flexDirection: 'row'
-    },
-    [theme.breakpoints.up('lg')]: {
-      display: "none",
+	hideNav: {
+		padding: theme.spacing(1),
+		[theme.breakpoints.down('sm')]: {
+			display: 'flex',
+			flexDirection: 'row',
+			justifyContent: 'space-between',
+		},
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+			flexDirection: 'row',
+		},
+		[theme.breakpoints.up('lg')]: {
+			display: 'none',
+		},
+	},
 
-    }
-  },
-
-  menuIcon: {
-    color: 'white',
-    '&:hover': {
-      color: '#ee4d71'
-    }
-  }
-
+	menuIcon: {
+		color: 'white',
+		'&:hover': {
+			color: '#ee4d71',
+		},
+	},
 }));
 
 export default function NavMenu() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const history = useHistory();
+	const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
+	const handleClick = event => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-
-
-  return (
-    <div className={classes.hideNav}>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} className={classes.menuIcon}>
-        <MenuIcon />
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>About Us</MenuItem>
-        <MenuItem onClick={handleClose}>Features</MenuItem>
-        <MenuItem onClick={auth.login}>Get Started</MenuItem>
-
-      </Menu>
-    </div>
-  );
+	return (
+		<div className={classes.hideNav}>
+			<Button
+				aria-controls="simple-menu"
+				aria-haspopup="true"
+				onClick={handleClick}
+				className={classes.menuIcon}>
+				<MenuIcon />
+			</Button>
+			<Menu
+				id="simple-menu"
+				anchorEl={anchorEl}
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={handleClose}>
+				<MenuItem onClick={handleClose}>About Us</MenuItem>
+				<MenuItem onClick={handleClose}>Features</MenuItem>
+				<MenuItem onClick={auth.login}>Get Started</MenuItem>
+			</Menu>
+		</div>
+	);
 }
