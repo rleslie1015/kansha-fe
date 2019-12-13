5d350a8232b8a4f2bca09384b585e4768f2b8921
Matt M <matt.d.m@hotmail.com>
2019-12-13 15:03:56 -0600

    ⚛ added reactions for redux

--- react-app/src/store/reducers/feed-reducer.js
+++ react-app/src/store/reducers/feed-reducer.js
@@ -2,15 +2,21 @@ import {
 	FEED_LOAD_START,
 	FEED_LOAD_SUCCESS,
 	FEED_LOAD_FAILURE,
-    FEED_EVENT_NEW_REC,
-    POST_DATA_LOAD_SUCCESS,
-    POST_DATA_LOAD_FAILURE,
+	FEED_EVENT_NEW_REC,
+	POST_DATA_LOAD_SUCCESS,
+	POST_DATA_LOAD_FAILURE,
+	FEED_REACT_START,
+	FEED_REACT_SUCCESS,
+	FEED_REACT_FAILURE,
+	FEED_REACT_REMOVE_START,
+	FEED_REACT_REMOVE_SUCCESS,
+	FEED_REACT_REMOVE_FAILURE,
 } from '../actions/feed-actions';
 
 const initialState = {
-    feed: [],
-    reactions: {},
-    comments: {},
+	feed: [],
+	reactions: {},
+	comments: {},
 	isLoading: false,
 };
 
@@ -37,13 +43,51 @@ export const feedReducer = (state = initialState, action) => {
 			return {
 				...state,
 				feed: [...action.payload, ...state.feed],
-            };
-        case POST_DATA_LOAD_SUCCESS:
-            let { id, type } = action.data;
-            return {
-                ...state,
-                [type]: {...state[type], [id]: action.payload} 
-            }
+			};
+		case POST_DATA_LOAD_SUCCESS:
+			let { id, type } = action.data;
+			return {
+				...state,
+				[type]: { ...state[type], [id]: action.payload },
+			};
+		case POST_DATA_LOAD_FAILURE:
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+			};
+		case FEED_REACT_START:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case FEED_REACT_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+			};
+		case FEED_REACT_FAILURE:
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+			};
+		case FEED_REACT_REMOVE_START:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case FEED_REACT_REMOVE_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+			};
+		case FEED_REACT_REMOVE_FAILURE:
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+			};
 		default:
 			return state;
 	}
