ea9afd5a3074827cd1fd0ec933bc9b8cf4717011
Matt M <matt.d.m@hotmail.com>
2019-12-15 23:03:47 -0600

    📂 fixed assets directory

--- react-app/src/Workspace_Card.jsx
+++ react-app/src/Workspace_Card.jsx
@@ -6,7 +6,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import RecogModal from './Components/RecogModal/RecogModal';
-import trashcan from './assests/Trashcan.png';
+import trashcan from './assets/Trashcan.png';
 import { axiosWithAuth } from './utils/axiosWithAuth';
 
 const useStyles = makeStyles(theme => ({
