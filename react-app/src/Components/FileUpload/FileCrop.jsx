b747ce565a8ddcb53e3978c2148fb3e0c974c9a8
Joscelyn <JoscelynRose@gmail.com>
2020-02-18 18:41:30 -0500

    removed material ui from FileUpload
    Co-authored-by: ackers93 <andrew.ackers@gmail.com>
    Co-authored-by: Hail91 <aarongillies88@gmail.com>

--- react-app/src/Components/FileUpload/FileCrop.jsx
+++ react-app/src/Components/FileUpload/FileCrop.jsx
@@ -2,95 +2,13 @@ import React, { useState } from 'react';
 import ReactCrop from 'react-image-crop';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { uploadPicture, uploadBadContent } from '../../store/actions/user-actions';
+import {
+	uploadPicture,
+	uploadBadContent,
+} from '../../store/actions/user-actions';
 import 'react-image-crop/dist/ReactCrop.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import { useSpring, animated } from 'react-spring/web.cjs';
-import { Button } from '@material-ui/core';
-
-const useStyles = makeStyles(theme => ({
-      modal: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	paper: {
-		backgroundColor: '#2D2C35',
-		border: '2px solid #000',
-		boxShadow: 'none',
-		padding: theme.spacing(2, 4, 3),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-        color: 'white',
-    },
-    button: {
-        background: 'linear-gradient(135deg, #EE4D71 0%, #F15A3F 100%)',
-        opacity: '1',
-        '&:hover': {
-            color: 'white',
-            transition: '0.5s ease',
-          },
-    }, 
-    cancelButton: {
-		alignSelf: 'flex-start',
-		cursor: 'pointer',
-	},
-    cameraButton: {
-        opacity: 0,
-        '&:hover': {
-             opacity: 1,
-             boxShadow: 'none',
-            background: 'rgba(18, 18, 18, 0.85)',
-             transition: '0.5s ease',
-          },
-        [theme.breakpoints.down('sm')]: {
-			borderRadius: '100%',
-			// Hard coding until we can make a circle img cropper for users
-			width: '150px',
-			height: '150px',
-			background: 'linear-gradient(135deg, #EE4D71 0%, #F15A3F 100%)',
-			objectFit: 'cover',
-			objectPosition: '50% 50%',
-
-        },
-		[theme.breakpoints.up('md')]: {
-			borderRadius: '100%',
-			// Hard coding until we can make a circle img cropper for users
-			width: '250px',
-			height: '250px',
-			background: 'linear-gradient(135deg, #EE4D71 0%, #F15A3F 100%)',
-			objectFit: 'cover',
-			objectPosition: '50% 50%',
-
-        },
-        [theme.breakpoints.up('lg')]: {
-			borderRadius: '100%',
-			// Hard coding until we can make a circle img cropper for users
-			width: '272px',
-			height: '272px',
-			background: 'linear-gradient(135deg, #EE4D71 0%, #F15A3F 100%)',
-			objectFit: 'cover',
-			objectPosition: '50% 50%',
-			
-		}
-    },
-	camera: {
-        marginBottom: '10px',
-        marginRight: '10px',
-		width: '60%',
-        height: 'auto',
-    },
-    picture: {
-        display: 'flex',
-        justifyContent: 'center',
-        maxHeight: '800px',
-        maxWidth: '800px',
-        width: '100%',
-    },
-}));
 
 const Fade = React.forwardRef(function Fade(props, ref) {
 	const { in: open, children, onEnter, onExited, ...other } = props;
@@ -127,180 +45,183 @@ export const Cropper = connect(({ user }) => ({ ...user }), {
 	uploadPicture,
 	uploadBadContent,
 })(({ uploadPicture, uploadBadContent }) => {
+	const [open, setOpen] = useState(false);
+	const [imageRef, setImageRef] = useState();
+	const [file, setFile] = useState();
+	const [src, setSrc] = useState({});
+	const [croppedImg, setCroppedImg] = useState();
+	const [tool, setTool] = useState({
+		crop: {
+			unit: '%',
+			width: '60',
+			aspect: 1 / 1,
+		},
+	});
 
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const [ imageRef, setImageRef ] = useState()
-    const [ file, setFile ] = useState()
-    const [ src, setSrc ] = useState({})
-    const [ croppedImg, setCroppedImg ] = useState()
-    const [ tool, setTool ] = useState({
-         crop: {
-            unit: '%',
-            width: '60',
-            aspect: 1/1,
-    }});
+	const resetState = () => {
+		setImageRef();
+		setFile();
+		setSrc({});
+		setCroppedImg();
+		setTool({
+			crop: {
+				unit: '%',
+				width: '60',
+				aspect: 1 / 1,
+			},
+		});
+	};
 
-    const resetState = () => {
-        setImageRef()
-        setFile()
-        setSrc({})
-        setCroppedImg()
-        setTool({
-            crop: {
-                unit: '%',
-                width: '60',
-               aspect: 1/1,
-       }})
-    }
-
-    const handleOpen = () => {
+	const handleOpen = () => {
 		setOpen(true);
 	};
 
 	const handleClose = () => {
-        setOpen(false);
-        resetState()
-    };
-    
-    const imageTypes = types => {
-        return types.map(type => `image/${type}`);
-    };
-    
-    const handleUpload = e => {
-        const allowedImageTypes = imageTypes(['png', 'jpeg', 'gif']);
-        if (allowedImageTypes.includes(file.type)) {
-            const data = new FormData();
-            data.append('image', file);
-            uploadPicture(data);
-            handleClose();
-        } else {
-            uploadBadContent(file.type, allowedImageTypes);
-        }
-    };
+		setOpen(false);
+		resetState();
+	};
 
-    const onSelectFile = e => { 
-        if (e.target.files && e.target.files.length > 0) {
-            const reader = new FileReader();
-            reader.addEventListener('load', () => 
-                setSrc(reader.result))
-            reader.readAsDataURL(e.target.files[0])
-    }
-    }
-    
-    const onImageLoaded = image => {
-        setImageRef(image)
-        makeClientCrop(tool.crop)
-    }
-    
-    const onCropComplete = crop => {
-        makeClientCrop(crop)
-    }
-    
-    const onCropChange = (crop) => {
-        setTool({ crop })
-    }
+	const imageTypes = types => {
+		return types.map(type => `image/${type}`);
+	};
 
-    const makeClientCrop = async (crop) => {
-        if (imageRef && crop.width && crop.height) {
-            const croppedImageUrl = await getCroppedImg(
-                imageRef,
-                crop,
-                'newFile.jpg'
-            );
-            setCroppedImg(croppedImageUrl)
-        }
-    }
+	const handleUpload = e => {
+		const allowedImageTypes = imageTypes(['png', 'jpeg', 'gif']);
+		if (allowedImageTypes.includes(file.type)) {
+			const data = new FormData();
+			data.append('image', file);
+			uploadPicture(data);
+			handleClose();
+		} else {
+			uploadBadContent(file.type, allowedImageTypes);
+		}
+	};
 
-    const getCroppedImg = (image, crop, fileName) => {
-        const canvas = document.createElement('canvas');
-        const scaleX = image.naturalWidth / image.width;
-        const scaleY = image.naturalHeight / image.height;
-        canvas.width = crop.width;
-        canvas.height = crop.height;
-        const ctx = canvas.getContext('2d');
+	const onSelectFile = e => {
+		if (e.target.files && e.target.files.length > 0) {
+			const reader = new FileReader();
+			reader.addEventListener('load', () => setSrc(reader.result));
+			reader.readAsDataURL(e.target.files[0]);
+		}
+	};
 
-        ctx.drawImage(
-            image,
-            crop.x * scaleX,
-            crop.y * scaleY,
-            crop.width * scaleX,
-            crop.height * scaleY,
-            0,
-            0,
-            crop.width,
-            crop.height
-        )
+	const onImageLoaded = image => {
+		setImageRef(image);
+		makeClientCrop(tool.crop);
+	};
 
-        return new Promise((resolve) => {
-            let reader = new FileReader();
-            canvas.toBlob(blob => {
-                if (!blob) {
-                    return
-                }
-                blob.name = fileName;
-                window.URL.revokeObjectURL(croppedImg)
-                let fileUrl = (window.URL.createObjectURL(blob))
-                resolve(fileUrl)
-                reader.readAsDataURL(blob)
-                setFile(blob)
-            }, 'image/jpeg');
-        })
-}
+	const onCropComplete = crop => {
+		makeClientCrop(crop);
+	};
 
-    return (
-        <div class='crop' className={classes.root}>
-            <Button
-                type="button"
-                disableFocusRipple
-                disableRipple
+	const onCropChange = crop => {
+		setTool({ crop });
+	};
+
+	const makeClientCrop = async crop => {
+		if (imageRef && crop.width && crop.height) {
+			const croppedImageUrl = await getCroppedImg(
+				imageRef,
+				crop,
+				'newFile.jpg',
+			);
+			setCroppedImg(croppedImageUrl);
+		}
+	};
+
+	const getCroppedImg = (image, crop, fileName) => {
+		const canvas = document.createElement('canvas');
+		const scaleX = image.naturalWidth / image.width;
+		const scaleY = image.naturalHeight / image.height;
+		canvas.width = crop.width;
+		canvas.height = crop.height;
+		const ctx = canvas.getContext('2d');
+
+		ctx.drawImage(
+			image,
+			crop.x * scaleX,
+			crop.y * scaleY,
+			crop.width * scaleX,
+			crop.height * scaleY,
+			0,
+			0,
+			crop.width,
+			crop.height,
+		);
+
+		return new Promise(resolve => {
+			let reader = new FileReader();
+			canvas.toBlob(blob => {
+				if (!blob) {
+					return;
+				}
+				blob.name = fileName;
+				window.URL.revokeObjectURL(croppedImg);
+				let fileUrl = window.URL.createObjectURL(blob);
+				resolve(fileUrl);
+				reader.readAsDataURL(blob);
+				setFile(blob);
+			}, 'image/jpeg');
+		});
+	};
+
+	return (
+		<div class="crop" className="root">
+			<button
+				type="button"
+				disableFocusRipple
+				disableRipple
 				onClick={handleOpen}
-				className={classes.cameraButton}>
-					<img
-                        src= 'https://kansha-bucket.s3-us-west-1.amazonaws.com/cameraicon.png'
-                        className={classes.camera}
-						alt="upload icon"
-					/>
-			</Button>
-            <Modal				
-            className={classes.modal}
+				className="camera-button">
+				<img
+					src="https://kansha-bucket.s3-us-west-1.amazonaws.com/cameraicon.png"
+					className="camera"
+					alt="upload icon"
+				/>
+			</button>
+			<div
+				className="modal"
 				open={open}
 				onClose={handleClose}
 				closeAfterTransition
 				BackdropComponent={Backdrop}
 				BackdropProps={{
-				    timeout: 500,
-			}}>
-            <Fade in={open}>
-                <div className={classes.paper}>
-					<img
-					src="https://kansha-bucket.s3-us-west-1.amazonaws.com/x.png"
-					onClick={handleClose}
-                    className={classes.cancelButton}
-                    alt="close button"
+					timeout: 500,
+				}}>
+				<Fade in={open}>
+					<div className="paper">
+						<img
+							src="https://kansha-bucket.s3-us-west-1.amazonaws.com/x.png"
+							onClick={handleClose}
+							className="cancel-button"
+							alt="close button"
 						/>
-                    <input type="file" accept="image/*" onChange={onSelectFile} />   
-                    <div className={classes.picture}>
-                    {src && (
-                    <ReactCrop
-                        src={src}
-                        crop={tool.crop}
-                        circularCrop
-                        onImageLoaded={onImageLoaded}
-                        onComplete={onCropComplete}
-                        onChange={onCropChange}
-                    />
-                    )}
-                    </div>
-                <Button
-                	type="button"
-				    onClick={handleUpload}
-				    className={classes.button}>
-				        Upload
-			</Button>
-                </div>
-            </Fade>
-            </Modal>
-        </div>
-    )
-})
+						<input
+							type="file"
+							accept="image/*"
+							onChange={onSelectFile}
+						/>
+						<div className="picture">
+							{src && (
+								<ReactCrop
+									src={src}
+									crop={tool.crop}
+									circularCrop
+									onImageLoaded={onImageLoaded}
+									onComplete={onCropComplete}
+									onChange={onCropChange}
+								/>
+							)}
+						</div>
+						<button
+							type="button"
+							onClick={handleUpload}
+							className="button">
+							Upload
+						</button>
+					</div>
+				</Fade>
+			</div>
+		</div>
+	);
+});
--- react-app/src/Components/FileUpload/FileUpload.jsx
+++ react-app/src/Components/FileUpload/FileUpload.jsx
@@ -4,21 +4,6 @@ import {
 	uploadPicture,
 	uploadBadContent,
 } from '../../store/actions/user-actions';
-import { makeStyles } from '@material-ui/core/styles';
-
-
-const useStyles = makeStyles(theme => ({
-	input: {
-		opacity: 0,
-		borderRadius: '100%',
-		width: '100%',
-		position: 'absolute',
-		top: '0',
-		left: '0',
-		height: '100%',
-		cursor: 'pointer',
-	},
-}));
 
 const imageTypes = types => {
 	return types.map(type => `image/${type}`);
@@ -28,8 +13,6 @@ export const FileUpload = connect(({ user }) => ({ ...user }), {
 	uploadPicture,
 	uploadBadContent,
 })(({ uploadPicture, uploadBadContent, uploadError }) => {
-	const classes = useStyles();
-
 	const HandleUpload = e => {
 		const file = e.target.files[0];
 		const allowedImageTypes = imageTypes(['png', 'jpeg', 'gif']);
@@ -40,12 +23,9 @@ export const FileUpload = connect(({ user }) => ({ ...user }), {
 		} else {
 			uploadBadContent(file.type, allowedImageTypes);
 		}
-    };
+	};
 
 	return (
-		<input
-			className={classes.input}
-			onChange={HandleUpload}
-			type="file"></input>
+		<input className="input" onChange={HandleUpload} type="file"></input>
 	);
 });
