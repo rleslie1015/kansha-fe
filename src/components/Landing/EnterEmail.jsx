59efe354f54264883c9c0ce2103093e9d83b7f87
Joscelyn <JoscelynRose@gmail.com>
2020-03-02 17:12:00 -0500

    fixed up get started button on bottom of page

--- src/components/Landing/EnterEmail.jsx
+++ src/components/Landing/EnterEmail.jsx
@@ -4,11 +4,6 @@ const auth = new Auth();
 export default function EnterEmail() {
 	return (
 		<form className="container-enter-email">
-			<label htmlFor="landing-email">Enter your work email address</label>
-			<input
-				placeholder="Enter your work email address"
-				id="landing-email"
-			/>
 			<button onClick={auth.login}>Get Started</button>
 		</form>
 	);
