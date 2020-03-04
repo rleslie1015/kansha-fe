f85dcd862cc512945a92355cbf63f3f555b39a7b
Joscelyn <JoscelynRose@gmail.com>
2020-03-04 18:17:21 -0500

    set up architecture for reports page
    Co-authored-by: vtellez1 <vanessatellez45@gmail.com>
    Co-authored-by: emilybruner <bruner.emilym@gmail.com>
    Co-authored-by: Hail91 <aarongillies88@gmail.com>
    Co-authored-by: AndrewMaddocks <andrewmadd@icloud.com>

--- src/components/AdminDashboard/Reports.jsx
+++ src/components/AdminDashboard/Reports.jsx
@@ -4,6 +4,12 @@ export default function Reports() {
 	return (
 		<div className="reports">
 			<h1 className="admin-title">Reports</h1>
+			<main>
+				<section className="container-participation-donut-chart"></section>
+				<section className="container-thanked-leaderboard"></section>
+				<section className="container-team-engagement"></section>
+				<section className="container-thankful-engagement"></section>
+			</main>
 		</div>
 	);
 }
