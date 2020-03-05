5838f35458cf23f7c9e295238d6b8c5822e59b14
Andrew Maddocks <andrewmadd@icloud.com>
2020-03-05 13:20:56 -0700

    fixed bug when sending recognitons
    Co-authored-by: Emily Bruner <bruner.emilym@gmail.com>
    Co-authored-by: Vanessa Tellez <vanessatellez45@gmail.com>

--- src/components/AdminDashboard/reports/MostRecog.jsx
+++ src/components/AdminDashboard/reports/MostRecog.jsx
@@ -1,17 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { axiosWithAuth } from '../../../utils/axiosWithAuth';
 
 function MostRecog() {
+	const [filter, setFilter] = useState('');
+	const [rank, setRank] = useState([]);
+
 	useEffect(() => {
+		// setFilter('days');
+
 		axiosWithAuth()
-			.get('/reports/topemployees?time=years')
+			.get(`/reports/topemployees?time=days`)
 			.then(res => {
-				console.log(res);
+				// console.log(res);
+				setRank(res.data);
+				console.log(rank, 'Rank');
+				console.log(res.data, 'res.data');
 			})
 			.catch(err => {
 				console.log(err);
 			});
 	}, []);
+
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			setFilter('days');
+	// 			const { data } = await axiosWithAuth().get(
+	// 				`/reports/topemployees?time=${filter}`,
+	// 			);
+	// 			await setRank(data);
+	// 			console.log(data, 'data');
+	// 			console.log('this is rank', rank);
+	// 		} catch (err) {
+	// 			console.error(err);
+	// 		}
+	// 	};
+	// 	fetchData();
+	// }, [filter]);
+
 	return (
 		<div>
 			<h1>andrew was here</h1>
