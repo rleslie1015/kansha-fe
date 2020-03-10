ccc8e94c8b14402ae2f0658d2f9534e6fa8d791f
Joscelyn <JoscelynRose@gmail.com>
2020-03-10 13:27:31 -0400

    created a second doughnut chart
    Co-authored-by: Hail91 <aarongillies88@gmail.com>

--- src/components/AdminDashboard/Reports.jsx
+++ src/components/AdminDashboard/Reports.jsx
@@ -1,40 +1,23 @@
 import React from 'react';
-import MostRecog from './reports/MostRecog';
-<<<<<<< HEAD
-import MostRecogGiven from './reports/MostRecogGiven';
-
-||||||| merged common ancestors
-=======
 import TotalRecogGraph from './reports/TotalRecogGraph';
-import TotalRecognitionChart from './reports/TotalRecognitionChart';
+import DoughnutCharts from './reports/DoughnutCharts';
 
->>>>>>> e7ef5a6ffa6e4e0b6e16cb52d9b49dc9c4867c60
 export default function Reports() {
 	return (
 		<div className="reports">
-			<h1 className="admin-title">Reports</h1>
+			<h1 className="admin-title">Overview</h1>
 			<main>
+				<h2>Analytics</h2>
 				<section className="container-total-recog-graph">
 					<h1>Recognition</h1>
 					<TotalRecogGraph />
 				</section>
-				<section className="container-thanked-leaderboard">
-					<MostRecog />
-				</section>
-<<<<<<< HEAD
-				<section className="container-team-engagement"></section>
+				<section className="container-thanked-leaderboard"></section>
 				<section className="container-thankful-engagement">
-					<MostRecogGiven />
+					<h1>Participation</h1>
+					<DoughnutCharts />
 				</section>
-||||||| merged common ancestors
 				<section className="container-team-engagement"></section>
-				<section className="container-thankful-engagement"></section>
-=======
-				<section className="container-team-engagement">
-					<TotalRecognitionChart />
-				</section>
-				<section className="container-thankful-engagement"></section>
->>>>>>> e7ef5a6ffa6e4e0b6e16cb52d9b49dc9c4867c60
 			</main>
 		</div>
 	);
--- src/components/AdminDashboard/reports/MostRecog.jsx
+++ src/components/AdminDashboard/reports/MostRecog.jsx
@@ -14,88 +14,12 @@ function MostRecog() {
 		axiosWithAuth()
 			.get(`/reports/top?type=recipient&limit=5&time=${filter}`)
 			.then(res => {
-<<<<<<< HEAD
-				setRank(res.data.employees);
-				setCount(res.data.count);
-				console.log(res);
-||||||| merged common ancestors
-				console.log(res);
-=======
 				// console.log(res);
->>>>>>> e7ef5a6ffa6e4e0b6e16cb52d9b49dc9c4867c60
 			})
 			.catch(err => {
 				console.log(err);
 			});
-<<<<<<< HEAD
-	}, [filter]);
-
-	const handleTime = e => {
-		e.preventDefault();
-		setFilter(e.target.value);
-	};
-
-	return (
-		<div className="most-recog-container">
-			<select
-				onChange={handleTime}
-				style={{
-					border: 'none',
-					fontWeight: 'bold',
-					color: 'black',
-					fontSize: '2rem',
-					width: '25rem',
-				}}
-				id="cars">
-				<option value="weeks">Weekly Leader Board</option>
-				<option value="months">Monthly Leader Board</option>
-				<option value="years">Yearly Leader Board</option>
-			</select>
-
-			{rank.map(person => {
-				return (
-					<div
-						className="most-recog-individual"
-						key={person.recipient}>
-						<img
-							className="most-rec-profile-pic"
-							src={person.profile_picture}
-							alt="most thanked individual"></img>
-						<div className="most-recog-name">
-							<p>{`${
-								person.first_name
-							} ${person.last_name[0].toUpperCase()}`}</p>
-						</div>
-						<div className="most-recog-progress">
-							<progress
-								value={person.count}
-								max={count}></progress>
-						</div>
-						<div className="most-recog-heart">
-							<p>
-								{person.count}
-								<img
-									className="heart-img"
-									src={heart}
-									alt="heart icon"
-								/>
-							</p>
-						</div>
-					</div>
-				);
-			})}
-		</div>
-	);
-||||||| merged common ancestors
-	}, []);
-	return (
-		<div>
-			<h1>andrew was here</h1>
-		</div>
-	);
-=======
 	}, []);
 	return <div></div>;
->>>>>>> e7ef5a6ffa6e4e0b6e16cb52d9b49dc9c4867c60
 }
 export default MostRecog;
