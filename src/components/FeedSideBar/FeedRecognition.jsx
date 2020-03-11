1f1496a1b6bee033046def9db3050a50dc871250
bamfranceschi <anna.franceschi@gmail.com>
2020-03-11 11:19:35 -0500

    feed bar edits
    Co-authored-by: ackers93 <andrew.ackers@gmail.com>
    Co-authored-by: AndrewMaddocks <andrewmadd@icloud.com>
    Co-authored-by: keveightysev <keveightysev@gmail.com>

--- src/components/FeedSideBar/FeedRecognition.jsx
+++ src/components/FeedSideBar/FeedRecognition.jsx
@@ -15,11 +15,15 @@ function FeedRecognition({ rec, badge, open }) {
 		<div className="recognition" onClick={handleClick}>
 			<img className="rec-profile-pic" src={rec.recipient_picture} />
 			<div className="rec-badge-and-message">
-				<img
-					className={`${open ? `rec-badge` : `hidden-rec`}`}
-					src={badge?.badge_URL}
-				/>
-				<p className={`${open ? `rec-message` : `hidden-rec`}`}>
+				{badge && (
+					<img
+						className={`${open ? `rec-badge` : `hidden-rec`}`}
+						src={badge?.badge_URL}
+					/>
+				)}
+				<p
+					style={{ paddingLeft: !badge && '10px' }}
+					className={`${open ? `rec-message` : `hidden-rec`}`}>
 					{rec?.message}
 				</p>
 			</div>
