c17f863ae10fb8bc51e0f8f167df9a6d8043216f
Joscelyn <JoscelynRose@gmail.com>
2020-03-12 16:44:03 -0400

    WIP: fixing the reports tests

--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -1,27 +1,53 @@
-import React from 'react'
-import { Router} from 'react-router-dom';
-import {render} from '@testing-library/react';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
 export const findByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test="${val}"]`);
-}
+	return wrapper.find(`[data-test="${val}"]`);
+};
 
 export const renderWithRouter = (
-    ui,
-    {
-      route = '/',
-      history = createMemoryHistory({ initialEntries: [route] }),
-    } = {}
-  ) => {
-    const Wrapper = ({ children }) => (
-      <Router history={history}>{children}</Router>
-    )
-    return {
-      ...render(ui, { wrapper: Wrapper }),
-      // adding `history` to the returned utilities to allow us
-      // to reference it in our tests (just try to avoid using
-      // this to test implementation details).
-      history,
-    }
-  }
+	ui,
+	{
+		route = '/',
+		history = createMemoryHistory({ initialEntries: [route] }),
+	} = {},
+) => {
+	const Wrapper = ({ children }) => (
+		<Router history={history}>{children}</Router>
+	);
+	return {
+		...render(ui, { wrapper: Wrapper }),
+		// adding `history` to the returned utilities to allow us
+		// to reference it in our tests (just try to avoid using
+		// this to test implementation details).
+		history,
+	};
+};
+
+export const renderWithRouterAndRedux = (
+	ui,
+	{
+		route = '/',
+		history = createMemoryHistory({ initialEntries: [route] }),
+		initialState = { feed: { feed: [] } },
+	} = {},
+) => {
+	const mockStore = configureStore();
+	let store = mockStore(initialState);
+	const Wrapper = ({ children }) => (
+		<Provider store={store}>
+			<Router history={history}>{children}</Router>
+		</Provider>
+	);
+	return {
+		...render(ui, { wrapper: Wrapper }),
+		// adding `history` to the returned utilities to allow us
+		// to reference it in our tests (just try to avoid using
+		// this to test implementation details).
+		history,
+	};
+};
