0b679217669a78061ea127c6f20261a508527612
Aaron <aarongillies88@gmail.com>
2020-03-12 19:58:22 -0400

    now dynamically rendering name of user who is logged in when viewing modal

--- src/components/HelpModal/HelpModal.jsx
+++ src/components/HelpModal/HelpModal.jsx
@@ -6,13 +6,13 @@ const HelpModal = ({ title, content, setTitle, setContent, setHelpModal }) => {
 	return (
 		<div className="help-modal-container">
 			<HelpModalMenu setTitle={setTitle} setContent={setContent} />
-			{/* <Link
-				className="modal-dash-link"
-				onClick={() => setHelpModal(false)}
-				to="/dashboard">
-				Take me to my dashboard
-			</Link> */}
 			<div className="help-modal-content">
+				<Link
+					className="modal-dash-link"
+					onClick={() => setHelpModal(false)}
+					to="/dashboard">
+					Take me to my dashboard
+				</Link>
 				<h1>{title}</h1>
 				<p>{content}</p>
 			</div>
