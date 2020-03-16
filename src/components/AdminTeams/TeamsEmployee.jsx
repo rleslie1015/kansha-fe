4ca646ce8c23c86df331f43ca123285ca4d2a860
Aaron <aarongillies88@gmail.com>
2020-03-16 15:41:09 -0400

    wip: styling employees-teams page
    Co-authored-by: ackers93 <andrew.ackers@gmail.com>

--- src/components/AdminTeams/TeamsEmployee.jsx
+++ src/components/AdminTeams/TeamsEmployee.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
+// Icon Imports
+import { ReactComponent as HideIcon } from '../../assets/TeamsIcons/hide.svg';
+import { ReactComponent as DeleteIcon } from '../../assets/TeamsIcons/delete.svg';
+import { ReactComponent as GroupIcon } from '../../assets/TeamsIcons/Group.svg';
+
 const TeamsEmployee = props => {
 	return (
-		<div>
-			<h2>
-				{props.data.first_name} {props.data.last_name}
-			</h2>
-			<h2>{props.data.job_title}</h2>
+		<div className="teams-employee-card">
+			<div className="teams-employee">
+				<img
+					src={props.data.profile_picture}
+					alt="profile img"
+					className="teams-profile-picture"
+				/>
+				<h3>
+					{props.data.first_name} {props.data.last_name}
+				</h3>
+			</div>
+			<div className="teams-employee-details">
+				<h3 className="job-title">{props.data.job_title}</h3>
+				<h3 className="teams">Teams</h3>
+			</div>
+			<div className="teams-employee-icons">
+				<HideIcon style={{ marginRight: '2rem' }} />
+				<DeleteIcon style={{ marginRight: '2rem' }} />
+				<GroupIcon />
+			</div>
 		</div>
 	);
 };
